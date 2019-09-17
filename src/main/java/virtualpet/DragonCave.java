0e0595c4b061141fa3ebd85f8f475586fbad5c59
Brendin Headlee <Brendin.Headlee24@gmail.com>
2019-09-17 14:56:29 -0400

    Fixed Shelters Do Magic

--- src/main/java/virtualpet/DragonCave.java
+++ src/main/java/virtualpet/DragonCave.java
@@ -11,11 +11,9 @@ public class DragonCave {
 
 	public DragonCave() {
 		petList = new HashMap<>();
-		
 
 	}
-	
-	
+
 	public void addPetToShelter(Dragon petToAdd) {
 		petList.put(petToAdd.getName(), petToAdd);
 
@@ -24,78 +22,97 @@ public class DragonCave {
 	public Collection<Dragon> retrievePetList() {
 		return petList.values();
 	}
-	
+
 	public Dragon retrieveVirtualpet(String petName) {
 		return petList.get(petName);
 	}
 
 	public void tickAllPets() {
-		for (Dragon pet: petList.values()) {
-//			pet.tick();
+		for (Dragon pet : petList.values()) {
+			// pet.tick();
 		}
-		
+
 	}
 
-//	public void feedAllPets(int amountFed) {
-//		for (Dragon pet: petList.values()) {
-//			pet.feedDragon(amountFed);
-//		}
-//		
-//	}
+	// public void feedAllPets(int amountFed) {
+	// for (Dragon pet: petList.values()) {
+	// pet.feedDragon(amountFed);
+	// }
+	//
+	// }
 
 	public void playWithAllPets(int timePlayed) {
-		for (Dragon pet: petList.values()) {
+		for (Dragon pet : petList.values()) {
 			pet.playWithDragon(timePlayed);
 		}
-		
+
 	}
 
 	public void restAllPets(int amountRested) {
-		for (Dragon pet: petList.values()) {
+		for (Dragon pet : petList.values()) {
 			pet.restDragon(amountRested);
 		}
-		
+
 	}
 
-//	public void doMagicWithAllPets(int breathFire) {
-//		for (Dragon pet: petList.values()) {
-//			pet.breathsFire(breathFire);
-//		}
-//		
-//	}
-	
+	// public void doMagicWithAllPets(int breathFire) {
+	// for (Dragon pet: petList.values()) {
+	// pet.breathsFire(breathFire);
+	// }
+	//
+	// }
+
 	public String[] retrieveAllPetAttributes() {
-		
+
 		String[] allPetAttributes = new String[petList.size()];
 		String arrayRow = "";
-		
-		for (Dragon pet: petList.values()) {
-			
-			arrayRow = (String.format("|%-10s",  pet.getName())) +
-//					(String.format("|%-10d",  pet.getHunger())) +
-//					(String.format("|%-10d",  pet.getFatigue())) +
-					(String.format("|%-10d",  pet.getBoredom())) +
-					(String.format("|%-10d|",  pet.getMagic()));
+
+		for (Dragon pet : petList.values()) {
+
+			arrayRow = (String.format("|%-10s", pet.getName())) +
+			// (String.format("|%-10d", pet.getHunger())) +
+			// (String.format("|%-10d", pet.getFatigue())) +
+					(String.format("|%-10d", pet.getBoredom())) + (String.format("|%-10d|", pet.getMagic()));
 		}
 		System.out.println(arrayRow);
 		return allPetAttributes;
 	}
 
-
 	public int getAsh() {
 		return ashBuildUp;
 	}
+
 	public void setAsh(int amount) {
 		this.ashBuildUp = amount;
 	}
 
+	public void doMagic(Dragon dragon,int amount) { 
+		if (dragon instanceof RoboticIceDragon) {
+			RoboticIceDragon icedragon = (RoboticIceDragon) dragon;
+			icedragon.blowBitterWind(amount);
+			setAsh(getAsh()-amount);
+		} else if (dragon instanceof OrganicIceDragon) {
+			OrganicIceDragon icedragon = (OrganicIceDragon) dragon;
+			icedragon.blowBitterWind(amount);
+			setAsh(getAsh()-amount);
+		}else 	if (dragon instanceof RoboticFireDragon) {
+			RoboticFireDragon firedragon = (RoboticFireDragon) dragon;
+			firedragon.breathFire(amount);
+			setIcicles(getIcicles()-amount);
+		} else if (dragon instanceof OrganicFireDragon) {
+			OrganicFireDragon firedragon = (OrganicFireDragon) dragon;
+			firedragon.breathFire(amount);
+			setIcicles(getIcicles()-amount);
+		}
 
-	public void doMagic(OrganicIceDragon dragon) {
-		dragon.blowBitterWind(2);
-		setAsh(8);
-		
-		
 	}
-	
-	
+
+	public int getIcicles() {
+		return icicleBuildUp;
+	}
+
+	public void setIcicles(int icicleBuildUp) {
+		this.icicleBuildUp = icicleBuildUp;
+	}
+
 }
--- src/test/java/virtualpet/DragonCaveTest.java
+++ src/test/java/virtualpet/DragonCaveTest.java
@@ -1,4 +1,5 @@
 package virtualpet;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -10,67 +11,90 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class DragonCaveTest {
-@Test
-public void shouldBeAbleToAddOrganicAndRobiticDragons() {
-	Dragon underTestOrganic = new OrganicDragon("TestOrganic");
-	Dragon underTestFireRobotic = new RoboticFireDragon("TestFireRobotic");
-	DragonCave underTestCave= new DragonCave();
-	underTestCave.addPetToShelter(underTestFireRobotic);
-	underTestCave.addPetToShelter(underTestOrganic); 
-	Collection<Dragon> addedPets = underTestCave.retrievePetList();
-	assertThat(addedPets, containsInAnyOrder(underTestFireRobotic , underTestOrganic));
-}
+	@Test
+	public void shouldBeAbleToAddOrganicAndRobiticDragons() {
+		Dragon underTestOrganic = new OrganicDragon("TestOrganic");
+		Dragon underTestFireRobotic = new RoboticFireDragon("TestFireRobotic");
+		DragonCave underTestCave = new DragonCave();
+		underTestCave.addPetToShelter(underTestFireRobotic);
+		underTestCave.addPetToShelter(underTestOrganic);
+		Collection<Dragon> addedPets = underTestCave.retrievePetList();
+		assertThat(addedPets, containsInAnyOrder(underTestFireRobotic, underTestOrganic));
+	}
 
-	
 	@Test
 	public void shouldNotAddDuplicatePetNames() {
 		Dragon underTestOrganic = new OrganicDragon("Bob");
 		Dragon underTestRobotic = new RoboticDragon("Bob");
-		DragonCave underTestCave= new DragonCave();
+		DragonCave underTestCave = new DragonCave();
 		underTestCave.addPetToShelter(underTestRobotic);
-		underTestCave.addPetToShelter(underTestOrganic); 
+		underTestCave.addPetToShelter(underTestOrganic);
 		Collection<Dragon> addedPets = underTestCave.retrievePetList();
-		
+
 		assertThat(underTestCave.petList.size(), is(1));
-		
+
 	}
-	
+
 	@Test
-	public void shouldBeAbleToReturnPetByName() { 
-	Dragon underTestOrganic = new OrganicDragon("TestOrganic");
-	Dragon underTestRobotic = new RoboticDragon("TestRobotic");
-	DragonCave underTestCave= new DragonCave();
-	underTestCave.addPetToShelter(underTestRobotic);
-	underTestCave.addPetToShelter(underTestOrganic); 
-	Dragon retrieved = underTestCave.retrieveVirtualpet("TestOrganic");
+	public void shouldBeAbleToReturnPetByName() {
+		Dragon underTestOrganic = new OrganicDragon("TestOrganic");
+		Dragon underTestRobotic = new RoboticDragon("TestRobotic");
+		DragonCave underTestCave = new DragonCave();
+		underTestCave.addPetToShelter(underTestRobotic);
+		underTestCave.addPetToShelter(underTestOrganic);
+		Dragon retrieved = underTestCave.retrieveVirtualpet("TestOrganic");
 		assertThat(retrieved, is(underTestOrganic));
 
 		Dragon retrievedVirtualPet2 = underTestCave.retrieveVirtualpet("TestRobotic");
-		assertThat(retrievedVirtualPet2, is(underTestRobotic)); } 
+		assertThat(retrievedVirtualPet2, is(underTestRobotic));
+	}
 
 	@Test
 	public void shouldBeAbleToPlayWithAllPets() throws Exception {
-		
+
 		Dragon underTestOrganic = new OrganicDragon("TestOrganic");
 		Dragon underTestRobotic = new RoboticDragon("TestRobotic");
-		DragonCave underTestCave= new DragonCave();
+		DragonCave underTestCave = new DragonCave();
 		underTestCave.addPetToShelter(underTestRobotic);
-		underTestCave.addPetToShelter(underTestOrganic); 
-		
+		underTestCave.addPetToShelter(underTestOrganic);
+
 		underTestCave.playWithAllPets(4);
 		assertThat(underTestRobotic.getBoredom(), is(6));
-		assertThat(underTestOrganic.getBoredom(), is(6));	
-}
-	@Test
-	public void shouldBeAbleToClearAsh () {
-		OrganicIceDragon underTestOrganicIce = new OrganicIceDragon ("TestFrosty");
-		DragonCave underTestCave= new DragonCave();
-		underTestCave.addPetToShelter(underTestOrganicIce);
-		
-		underTestCave.doMagic(underTestOrganicIce);
-		int expected = underTestCave.getAsh();
-		assertThat(expected, is (8));
-		
+		assertThat(underTestOrganic.getBoredom(), is(6));
 	}
 
+	@Test
+	public void shouldBeAbleToClearAsh() {
+		OrganicIceDragon underTestOrganicIce = new OrganicIceDragon("TestFrosty");
+		DragonCave underTestCave = new DragonCave();
+		underTestCave.addPetToShelter(underTestOrganicIce);
+
+		underTestCave.doMagic(underTestOrganicIce,2);
+		int expected = underTestCave.getAsh();
+		assertThat(expected, is(8));
+
+	}
+
+	@Test
+	public void shouldBeAbleToClearAsh2() {
+		RoboticIceDragon underTestRoboticIce = new RoboticIceDragon("TestFrosty");
+		DragonCave underTestCave = new DragonCave();
+		underTestCave.addPetToShelter(underTestRoboticIce);
+
+		underTestCave.doMagic(underTestRoboticIce,2);
+		int expected = underTestCave.getAsh();
+		assertThat(expected, is(8));
+	}
+	@Test
+	public void shouldBeAbleToClearIcicles() {
+		RoboticFireDragon underTestRoboticFire = new RoboticFireDragon("TestFlamey");
+		RoboticFireDragon underTestRoboticFire2 = new RoboticFireDragon("TestFlamey");
+		DragonCave underTestCave = new DragonCave();
+		underTestCave.addPetToShelter(underTestRoboticFire);
+		underTestCave.addPetToShelter(underTestRoboticFire2);
+		underTestCave.doMagic(underTestRoboticFire,2);
+		underTestCave.doMagic(underTestRoboticFire2,2);
+		int expected = underTestCave.getIcicles();
+		assertThat(expected, is(6));
+	}
 }
